913ff33d7a47014379254140bdca6d415ab76d63
Michael Zappa <michaelzappa@Michaels-MacBook-Pro-8.local>
2024-11-18 10:20:15 -0700

    support CNI status verb
    Signed-off-by: Michael Zappa <michael.zappa@gmail.com>

--- cni.go
+++ cni.go
@@ -140,6 +140,16 @@ func (c *libcni) Status() error {
 	if len(c.networks) < c.networkCount {
 		return ErrCNINotInitialized
 	}
+
+	// STATUS is only called for CNI Version 1.1.0 or greater. It is ignored for previous versions.
+	for _, v := range c.networks {
+		err := c.cniConfig.GetStatusNetworkList(context.Background(), v.config)
+
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
